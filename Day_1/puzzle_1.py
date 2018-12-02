a3c7df2ce1defa1e45391ecb613436bbf77b3862
Ben Walker <bwalke05@gmail.com>
2018-12-02 13:40:50 -0500

    Solve first puzzle of day 1 using Python

---
+++ Day_1/puzzle_1.py
@@ -0,0 +1,20 @@
+PUZZLE_INPUT = 'puzzle_1_input.txt'
+
+def getPuzzleInput(puzzleFile):
+  with open(puzzleFile) as fileInput:
+    return [line.rstrip('\n') for line in fileInput]
+
+def representInputAsNumeric(input):
+  return [int(value) for value in input]
+
+def calculateFrequency(changes):
+  frequency = 0
+  for change in changes:
+    frequency += change
+  return frequency
+
+puzzleInput = getPuzzleInput(PUZZLE_INPUT)
+numericInput = representInputAsNumeric(puzzleInput)
+frequency = calculateFrequency(numericInput)
+
+print(f'resulting frequency: {frequency}')
