c147e03fba088604b28d4dec8da15e5892241955
Ben Walker <bwalke05@gmail.com>
2018-12-02 19:18:56 -0500

    Solve puzzle 1 of day 2

--- Day_2/puzzle_1.py
+++ Day_2/puzzle_1.py
@@ -4,4 +4,27 @@ def getPuzzleInput(puzzleFile):
   with open(puzzleFile) as fileInput:
     return [line.rstrip('\n') for line in fileInput]
 
-print(getPuzzleInput(PUZZLE_INPUT))
+def checkIdForDouble(boxID):
+  for char in boxID:
+    if boxID.count(char) == 2:
+      return True
+  return False
+
+def checkIdForTriple(boxID):
+  for char in boxID:
+    if boxID.count(char) == 3:
+      return True
+  return False
+
+def computeChecksum(boxIDs):
+  numDoubles = 0
+  numTriples = 0
+  for ID in boxIDs:
+    if checkIdForDouble(ID):
+      numDoubles += 1
+    if checkIdForTriple(ID):
+      numTriples += 1
+  return numDoubles * numTriples
+
+boxIDs = getPuzzleInput(PUZZLE_INPUT)
+print(f'checksum for box IDs: {computeChecksum(boxIDs)}')
