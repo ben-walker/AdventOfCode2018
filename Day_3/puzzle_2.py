f85dbc6a290fff006d56ce5d1a41a28128aa75e2
Ben Walker <bwalke05@gmail.com>
2018-12-10 14:16:13 -0500

    Refactor puzzle 2 day 3

--- Day_3/puzzle_2.py
+++ Day_3/puzzle_2.py
@@ -1,63 +1,30 @@
-import numpy
-import re
+import numpy, re, time
 
 PUZZLE_INPUT = 'day_3_input.txt'
 SIDE_LENGTH = 1000
-HAS_OVERLAPS = {}
 
-FREE = 0
-OVERLAP = -1
-
-def getPuzzleInput(puzzleFile):
-  with open(puzzleFile) as fileInput:
-    return [line.rstrip('\n') for line in fileInput]
+def genClaims(claimFile):
+  with open(claimFile) as fileInput:
+    claims = [line.rstrip('\n') for line in fileInput]
+  return map(lambda claim: map(int, re.findall(r'\d+', claim)), claims)
 
 def getInitialFabric(sideLength):
-  return numpy.zeros((SIDE_LENGTH, SIDE_LENGTH), numpy.int32)
+  return numpy.zeros((sideLength, sideLength))
 
-def getClaimCoords(claim):
-  pattern = '#.* @ (.*): .*'
-  rawCoords = re.search(pattern, claim).group(1).split(',')
-  return tuple([int(i) for i in rawCoords])
+def claimizeFabric(fabric):
+  for id, x, y, width, height in genClaims(PUZZLE_INPUT):
+    fabric[x : x + width, y : y + height] += 1
 
-def getClaimSize(claim):
-  pattern = '#.* @ .*: (.*)'
-  rawSize = re.search(pattern, claim).group(1).split('x')
-  return tuple([int(i) for i in rawSize])
+def findNonOverlappingClaim(claimizedFabric):
+  for id, x, y, width, height in genClaims(PUZZLE_INPUT):
+    if numpy.all(claimizedFabric[x : x + width, y : y + height] == 1):
+      return id
 
-def getClaimID(claim):
-  pattern = '#(.*) @ .*: .*'
-  return int(re.search(pattern, claim).group(1))
-
-def updateOverlapTracker(claimID, patch):
-  HAS_OVERLAPS[claimID] = True
-  if patch in HAS_OVERLAPS:
-    HAS_OVERLAPS[patch] = True
-
-def updateFabric(fabric, coords, size, id):
-  x = coords[0]
-  y = coords[1]
-  width = size[0]
-  height = size[1]
-
-  HAS_OVERLAPS[id] = False
-  for i in range(y, y + height):
-    for j in range(x, x + width):
-      patch = fabric[i][j]
-      if patch != FREE: updateOverlapTracker(id, patch)
-      fabric[i][j] = id if patch == FREE else OVERLAP
-
-def claimizeFabric(claims, fabric):
-  for claim in claims:
-    coords = getClaimCoords(claim)
-    size = getClaimSize(claim)
-    id = getClaimID(claim)
-    updateFabric(fabric, coords, size, id)
-
-def findNonOverlappingClaim():
-  return list(HAS_OVERLAPS.keys())[list(HAS_OVERLAPS.values()).index(False)]
-
+t = time.process_time()
 fabric = getInitialFabric(SIDE_LENGTH)
-claims = getPuzzleInput(PUZZLE_INPUT)
-claimizeFabric(claims, fabric)
-print(f'non-overlapping claim ID: {findNonOverlappingClaim()}')
+claimizeFabric(fabric)
+targetID = findNonOverlappingClaim(fabric)
+elapsed = round(time.process_time() - t, 4)
+
+print(f'non-overlapping claim ID: {targetID}')
+print(f'in {elapsed} seconds')
