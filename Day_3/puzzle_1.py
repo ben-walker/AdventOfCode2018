e4d59d826aba1038dcc5a0c1ba8deb9508092c2a
Ben Walker <bwalke05@gmail.com>
2018-12-10 13:20:59 -0500

    Refactor puzzle 1 day 3

--- Day_3/puzzle_1.py
+++ Day_3/puzzle_1.py
@@ -1,65 +1,29 @@
-import numpy
-import re
+import numpy, re, time
 
 PUZZLE_INPUT = 'day_3_input.txt'
 SIDE_LENGTH = 1000
 
-CLAIM = '#'
-FREE = '.'
-OVERLAP = 'X'
-
 def getPuzzleInput(puzzleFile):
   with open(puzzleFile) as fileInput:
-    return [line.rstrip('\n') for line in fileInput]
+    claims = [line.rstrip('\n') for line in fileInput]
+  return map(lambda claim: map(int, re.findall(r'\d+', claim)), claims)
 
 def getInitialFabric(sideLength):
-  return numpy.full((SIDE_LENGTH, SIDE_LENGTH), FREE)
-
-def getClaimCoords(claim):
-  '''
-  Return a tuple holding the starting coordinates of
-  the claim, with the form:
-  (X inches from the left, Y inches from the top)
-  '''
-  pattern = '#.* @ (.*): .*'
-  rawCoords = re.search(pattern, claim).group(1).split(',')
-  return tuple([int(i) for i in rawCoords])
-
-def getClaimSize(claim):
-  '''
-  Return a tuple holding the size of the claim,
-  with the form:
-  (inches wide, inches tall)
-  '''
-  pattern = '#.* @ .*: (.*)'
-  rawSize = re.search(pattern, claim).group(1).split('x')
-  return tuple([int(i) for i in rawSize])
-
-def updateFabric(fabric, startX, startY, width, height):
-  '''
-  On completion, fabric will be a numpy 2D array with
-  the following properties:
-  - '.' denotes an unclaimed square inch
-  - '#' denotes a square inch claimed once
-  - 'X' denotes a square inch claimed multiple times (overlap)
-  '''
-  for i in range(startY, startY + height):
-    for j in range(startX, startX + width):
-      patch = fabric[i][j]
-      fabric[i][j] = CLAIM if patch == FREE else OVERLAP
+  return numpy.zeros((sideLength, sideLength))
 
 def claimizeFabric(claims, fabric):
-  for claim in claims:
-    coords = getClaimCoords(claim)
-    size = getClaimSize(claim)
-    updateFabric(fabric, coords[0], coords[1], size[0], size[1])
+  for id, x, y, width, height in claims:
+    fabric[x : x + width, y : y + height] += 1
 
 def determineOverlaps(fabric):
-  unique, counts = numpy.unique(fabric, return_counts = True)
-  fabricCounts = dict(zip(unique, counts))
-  return fabricCounts.get(OVERLAP)
+  return len(numpy.where(fabric > 1)[0])
 
+t = time.process_time()
 fabric = getInitialFabric(SIDE_LENGTH)
 claims = getPuzzleInput(PUZZLE_INPUT)
 claimizeFabric(claims, fabric)
-print(f'overlapping square inches of fabric: {determineOverlaps(fabric)}')
+overlaps = determineOverlaps(fabric)
+elapsed = round(time.process_time() - t, 4)
+
+print(f'overlapping square inches of fabric: {overlaps}')
+print(f'in {elapsed} seconds')
