92f7025bfabddc2b0fa4d841e8fc9a590f675f13
Ben Walker <bwalke05@gmail.com>
2018-12-11 12:53:01 -0500

    Refactor naming to be more pythonic

--- Day_3/puzzle_1.py
+++ Day_3/puzzle_1.py
@@ -3,26 +3,26 @@ import numpy, re, time
 PUZZLE_INPUT = 'day_3_input.txt'
 SIDE_LENGTH = 1000
 
-def getPuzzleInput(puzzleFile):
-  with open(puzzleFile) as fileInput:
-    claims = [line.rstrip('\n') for line in fileInput]
+def get_puzzle_input(puzzle_file):
+  with open(puzzle_file) as file_input:
+    claims = [line.rstrip('\n') for line in file_input]
   return map(lambda claim: map(int, re.findall(r'\d+', claim)), claims)
 
-def getInitialFabric(sideLength):
-  return numpy.zeros((sideLength, sideLength))
+def get_initial_fabric(side_length):
+  return numpy.zeros((side_length, side_length))
 
-def claimizeFabric(claims, fabric):
+def claimize_fabric(claims, fabric):
   for id, x, y, width, height in claims:
     fabric[x : x + width, y : y + height] += 1
 
-def determineOverlaps(fabric):
+def determine_overlaps(fabric):
   return len(numpy.where(fabric > 1)[0])
 
 t = time.process_time()
-fabric = getInitialFabric(SIDE_LENGTH)
-claims = getPuzzleInput(PUZZLE_INPUT)
-claimizeFabric(claims, fabric)
-overlaps = determineOverlaps(fabric)
+fabric = get_initial_fabric(SIDE_LENGTH)
+claims = get_puzzle_input(PUZZLE_INPUT)
+claimize_fabric(claims, fabric)
+overlaps = determine_overlaps(fabric)
 elapsed = round(time.process_time() - t, 4)
 
 print(f'overlapping square inches of fabric: {overlaps}')
--- Day_3/puzzle_2.py
+++ Day_3/puzzle_2.py
@@ -3,28 +3,28 @@ import numpy, re, time
 PUZZLE_INPUT = 'day_3_input.txt'
 SIDE_LENGTH = 1000
 
-def genClaims(claimFile):
-  with open(claimFile) as fileInput:
-    claims = [line.rstrip('\n') for line in fileInput]
+def gen_claims(claim_file):
+  with open(claim_file) as file_input:
+    claims = [line.rstrip('\n') for line in file_input]
   return map(lambda claim: map(int, re.findall(r'\d+', claim)), claims)
 
-def getInitialFabric(sideLength):
-  return numpy.zeros((sideLength, sideLength))
+def get_initial_fabric(side_length):
+  return numpy.zeros((side_length, side_length))
 
-def claimizeFabric(fabric):
-  for id, x, y, width, height in genClaims(PUZZLE_INPUT):
+def claimize_fabric(fabric):
+  for id, x, y, width, height in gen_claims(PUZZLE_INPUT):
     fabric[x : x + width, y : y + height] += 1
 
-def findNonOverlappingClaim(claimizedFabric):
-  for id, x, y, width, height in genClaims(PUZZLE_INPUT):
-    if numpy.all(claimizedFabric[x : x + width, y : y + height] == 1):
+def find_non_overlapping_claim(claimized_fabric):
+  for id, x, y, width, height in gen_claims(PUZZLE_INPUT):
+    if numpy.all(claimized_fabric[x : x + width, y : y + height] == 1):
       return id
 
 t = time.process_time()
-fabric = getInitialFabric(SIDE_LENGTH)
-claimizeFabric(fabric)
-targetID = findNonOverlappingClaim(fabric)
+fabric = get_initial_fabric(SIDE_LENGTH)
+claimize_fabric(fabric)
+target_id = find_non_overlapping_claim(fabric)
 elapsed = round(time.process_time() - t, 4)
 
-print(f'non-overlapping claim ID: {targetID}')
+print(f'non-overlapping claim ID: {target_id}')
 print(f'in {elapsed} seconds')
