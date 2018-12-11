a448a944ab5e1da874b0da406c2d823aa9249e45
Ben Walker <bwalke05@gmail.com>
2018-12-11 12:57:57 -0500

    Refactor naming to be more pythonic

--- Day_2/puzzle_1.py
+++ Day_2/puzzle_1.py
@@ -3,23 +3,22 @@ from collections import Counter
 
 PUZZLE_INPUT = 'day_2_input.txt'
 
-def getPuzzleInput(puzzleFile):
-  with open(puzzleFile) as fileInput:
-    return [line.rstrip('\n') for line in fileInput]
+def get_puzzle_input(puzzle_file):
+  with open(puzzle_file) as file_input:
+    return [line.rstrip('\n') for line in file_input]
 
-def computeChecksum(boxIDs):
-  twosCount = 0
-  threesCount = 0
-  for id in boxIDs:
-    commonLetters = Counter(id)
-    commonCounts = commonLetters.values()
-    if 2 in commonCounts: twosCount += 1
-    if 3 in commonCounts: threesCount += 1
-  return twosCount * threesCount
+def compute_checksum(box_ids):
+  twos_count = 0
+  threes_count = 0
+  for id in box_ids:
+    common_counts = Counter(id).values()
+    if 2 in common_counts: twos_count += 1
+    if 3 in common_counts: threes_count += 1
+  return twos_count * threes_count
 
 t = time.process_time()
-boxIDs = getPuzzleInput(PUZZLE_INPUT)
-checksum = computeChecksum(boxIDs)
+box_ids = get_puzzle_input(PUZZLE_INPUT)
+checksum = compute_checksum(box_ids)
 elapsed = round(time.process_time() - t, 4)
 
 print(f'checksum for box IDs: {checksum}')
--- Day_2/puzzle_2.py
+++ Day_2/puzzle_2.py
@@ -2,22 +2,22 @@ import itertools, time
 
 PUZZLE_INPUT = 'day_2_input.txt'
 
-def getPuzzleInput(puzzleFile):
-  with open(puzzleFile) as fileInput:
-    return [line.rstrip('\n') for line in fileInput]
+def get_puzzle_input(puzzle_file):
+  with open(puzzle_file) as file_input:
+    return [line.rstrip('\n') for line in file_input]
 
-def findCommonLettersForCorrectIDs(boxIDs):
-  targetLength = len(boxIDs[0]) - 1
-  for source, target in itertools.combinations(boxIDs, 2):
-    charAssociations = list(zip(source, target))
-    sameChars = [i[0] for i in charAssociations if i[0] == i[1]]
-    if len(sameChars) == targetLength:
-      return ''.join(sameChars)
+def find_common_letters_for_correct_ids(box_ids):
+  target_length = len(box_ids[0]) - 1
+  for source, target in itertools.combinations(box_ids, 2):
+    char_associations = list(zip(source, target))
+    same_chars = [i[0] for i in char_associations if i[0] == i[1]]
+    if len(same_chars) == target_length:
+      return ''.join(same_chars)
 
 t = time.process_time()
-boxIDs = getPuzzleInput(PUZZLE_INPUT)
-correctID = findCommonLettersForCorrectIDs(boxIDs)
+box_ids = get_puzzle_input(PUZZLE_INPUT)
+common_letters = find_common_letters_for_correct_ids(box_ids)
 elapsed = round(time.process_time() - t, 4)
 
-print(f'common letters between the correct IDs: {correctID}')
+print(f'common letters between the correct IDs: {common_letters}')
 print(f'in {elapsed} seconds')
