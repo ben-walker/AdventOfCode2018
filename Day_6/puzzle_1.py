d503b4694fe18e12c40fd9af54018e2f9747874e
Ben Walker <bwalke05@gmail.com>
2018-12-12 13:44:09 -0500

    Add manhattan distance calculation function

--- Day_6/puzzle_1.py
+++ Day_6/puzzle_1.py
@@ -5,6 +5,11 @@ PUZZLE_INPUT = 'day_6_input.txt'
 def get_points(puzzle_file):
   with open(puzzle_file) as file_input:
     points = [line.rstrip('\n') for line in file_input]
-  return map(lambda point: map(int, point.split(', ')), points)
+  return [tuple(map(int, point.split(', '))) for point in points]
+
+def calc_manhattan(M, P):
+  Mx, My = M
+  Px, Py = P
+  return abs(Mx - Px) + abs(My - Py)
 
 points = get_points(PUZZLE_INPUT)
